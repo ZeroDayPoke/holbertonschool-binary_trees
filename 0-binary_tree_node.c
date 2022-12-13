b66dd9fc8bf351f6596e3a491701134eeb21e6ff
Chris Stamper <cstampok@gmail.com>
2022-12-13 13:07:49 -0600

    add malloc fail handling for nice checker

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,13 +4,15 @@
  * binary_tree_node - creates new node for tree
  * @value: newNode intended value
  * @parent: parent node of node to be created
- * Return: pointer to newNode
+ * Return: pointer to newNode or NULL on fail
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
 	newNode = malloc(sizeof(binary_tree_t));
+	if (!(newNode))
+		return (NULL);
 	newNode->parent = parent;
 	newNode->n = value;
 	newNode->right = NULL;
