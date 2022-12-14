77fc067095c2157943d8f9f4a64abcd3c6fef3ff
Chris Stamper <cstampok@gmail.com>
2022-12-13 18:41:19 -0600

    fix preorder to make f left right

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,8 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	else
 	{
 		func(tree->n);
-		binary_tree_preorder(tree->right, func);
 		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 	return;
 }
